af36bb4b7b488fe8afd1bf05bc3fcc89c805c4c9
Miles Edland <edlandm@wwu.edu>
2014-08-08 13:10:07 -0700

    add script to recursively set up any submodules for a freshly cloned repo

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Run this when freshly cloning this repo, it sets up the submodules
+
+initmodule()
+{
+    for path in `cat .gitmodules | sed -n 's/^\s//; s/path = //p'`; do
+        git submodule init $path
+        git submodule update $path
+        pushd $path
+        git checkout master
+        # Recursively set up any nested subrepos
+        if [[ -f .gitmodules ]]; then
+            initmodule()
+        fi
+        popd
+    done
+}
+initmodule()
